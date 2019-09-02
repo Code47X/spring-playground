241eb1cbf6448e398f9f0ebc33e1f00d88f396e3
Tanner Hartwig <tanner.hartwig57@gmail.com>
2019-09-02 11:41:03 -0500

    Basic page header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import * as Styled from './AppStyles';
 import 'utility/baseline.css';
 
 const App = () => {
   return (
-    <div>
-      React-Spring Playground
-    </div>
+    <Styled.Header>
+      <Styled.HeadContent>
+        <Styled.Title>
+          React-Spring Playground
+        </Styled.Title>
+        <Styled.SubTitle>
+          A place to show off my component animations
+        </Styled.SubTitle>
+      </Styled.HeadContent>
+    </Styled.Header>
   );
-}
+};
 
 export default App;
---
+++ src/components/App/AppStyles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  width: 100%;
+  height: 500px;
+  background: linear-gradient(321.67deg, #7D4DAC 33.36%, #3377C8 110.33%);
+`;
+
+export const HeadContent = styled.div`
+  position: relative;
+  top: 45%;
+  width: 92%;
+  max-width: 1200px;
+  margin: auto;
+`;
+
+export const Title = styled.h1`
+  color: #FFFFFF;
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 3rem;
+  margin: 0;
+  margin-bottom: 8px;
+`;
+
+export const SubTitle = styled.h2`
+  color: #FFFFFF;
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
+  font-weight: lighter;
+  font-size: 1.6rem;
+  line-height: 44px;
+  margin: 0;
+`;
