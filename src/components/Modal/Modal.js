c961578035e1597cad6009595c9431774e5ef870
Tanner Hartwig <tanner.hartwig57@gmail.com>
2019-09-08 20:04:33 -0500

    More work on Modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,10 +7,13 @@ const Portal = ({ children }) => (
   ReactDOM.createPortal(children, document.getElementById('modal-portal'))
 );
 
-const Modal = ({ modalAnim, closeModal }) => (
+const Modal = ({ modalAnim, closeModal, pointerEvents }) => (
   <Styled.Modal
     onClick={closeModal}
-    style={{ background: modalAnim.bgA.to((bgA) => `rgba(0, 0, 0, ${bgA})`) }}
+    style={{
+      pointerEvents,
+      background: modalAnim.bgA.to((bgA) => `rgba(0, 0, 0, ${bgA})`)
+    }}
   >
     <Styled.ModalCard style={modalAnim}>
       <div className='modal-title'>
@@ -27,6 +30,7 @@ const ModalWrapper = ({ children }) => {
   const [open, setOpen] = useState(false);
   const openModal = () => setOpen(true);
   const closeModal = () => setOpen(false);
+  const pointerEvents = open ? 'all' : 'none';
 
   const transition = useTransition(open ? [1] : [], null, {
     from: {
@@ -51,7 +55,8 @@ const ModalWrapper = ({ children }) => {
         duration: key === 'bgA' ? 300 : key === 'opacity' ? 500 : undefined,
         friction: 12
       })
-    }
+    },
+    unique: true
   });
 
   return (
@@ -59,7 +64,11 @@ const ModalWrapper = ({ children }) => {
       {transition.map(
         ({ item, key, props: modalAnim }) =>
           <Portal key={key}>
-            <Modal modalAnim={modalAnim} closeModal={closeModal} />
+            <Modal
+              modalAnim={modalAnim}
+              closeModal={closeModal}
+              pointerEvents={pointerEvents}
+            />
           </Portal>
       )}
 
